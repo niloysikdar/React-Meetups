30b16d28af899ee06a19c7cf0889591d681bc564
niloysikdar <niloysikdar30@gmail.com>
2021-05-18 00:11:41 +0530

    Added each card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>Hello world</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <div className="card">
+        <h2>Title</h2>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
