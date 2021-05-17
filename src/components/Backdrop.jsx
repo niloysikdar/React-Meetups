65564b1949c9dbed6e575ba121cf9f4c6fa3e465
niloysikdar <niloysikdar30@gmail.com>
2021-05-18 01:06:40 +0530

    Added Modal function

---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,5 @@
+const Backdrop = () => {
+  return <div className="backdrop"></div>;
+};
+
+export default Backdrop;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 const Todo = ({ title }) => {
+  const [isModalOpen, setModalIsOpen] = useState(false);
+
+  const deleteHnadler = () => {
+    console.log(title);
+    setModalIsOpen(true);
+  };
+
   return (
     <div className="card">
       <h2>{title}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHnadler}>
+          Delete
+        </button>
       </div>
+      {isModalOpen && <Modal />}
+      {isModalOpen && <Backdrop />}
     </div>
   );
 };
