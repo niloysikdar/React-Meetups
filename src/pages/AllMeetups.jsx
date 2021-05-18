c5b39ba6f6a11d5977a85fc9ff8fc0570c64bc60
niloysikdar <niloysikdar30@gmail.com>
2021-05-18 12:42:35 +0530

    Added Basic Routing

---
+++ src/pages/AllMeetups.jsx
@@ -0,0 +1,9 @@
+const AllMeetupsPage = () => {
+  return (
+    <div>
+      <h2>All Meetups page</h2>
+    </div>
+  );
+};
+
+export default AllMeetupsPage;
---
+++ src/pages/Favourites.jsx
@@ -0,0 +1,9 @@
+const FavouritesPage = () => {
+  return (
+    <div>
+      <h2>This is Favourites Page</h2>
+    </div>
+  );
+};
+
+export default FavouritesPage;
