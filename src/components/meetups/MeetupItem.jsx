0ff8d0d9dbc35ad5c5fa264ff9fd71d6e52c2004
niloysikdar <niloysikdar30@gmail.com>
2021-05-18 21:44:49 +0530

    Added Final card Styling

--- src/components/meetups/MeetupItem.jsx
+++ src/components/meetups/MeetupItem.jsx
@@ -1,20 +1,23 @@
 import classes from "./MeetupItem.module.css";
+import Card from "../ui/Card";
 
 const MeetupItem = ({ meetup }) => {
   return (
-    <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={meetup.image} alt={meetup.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{meetup.title}</h3>
-        <address>{meetup.address}</address>
-        <p>{meetup.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>Add to Fav</button>
-      </div>
-    </li>
+    <Card>
+      <li className={classes.item}>
+        <div className={classes.image}>
+          <img src={meetup.image} alt={meetup.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{meetup.title}</h3>
+          <address>{meetup.address}</address>
+          <p>{meetup.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>Add to Fav</button>
+        </div>
+      </li>
+    </Card>
   );
 };
 
