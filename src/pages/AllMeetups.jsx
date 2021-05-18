1d77a6921acbb18abcbdfc18f4cd2a44caed3a06
niloysikdar <niloysikdar30@gmail.com>
2021-05-19 01:06:18 +0530

    Fetched data from Database

--- src/pages/AllMeetups.jsx
+++ src/pages/AllMeetups.jsx
@@ -1,32 +1,41 @@
 import MeetupList from "../components/meetups/MeetupList";
 import classes from "./AllMeetups.module.css";
-
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+import { useState } from "react";
+import { useEffect } from "react";
 
 const AllMeetupsPage = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  const fetchData = () => {
+    setLoading(true);
+    fetch(
+      "https://react-meetups-1eff3-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        console.log(meetups);
+        setLoadedMeetups(meetups);
+        setLoading(false);
+      });
+  };
+
+  useEffect(fetchData, []);
+
   return (
     <div className={classes.allmeetups}>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      {!isLoading ? <MeetupList meetups={loadedMeetups} /> : <p>Loading...</p>}
     </div>
   );
 };
