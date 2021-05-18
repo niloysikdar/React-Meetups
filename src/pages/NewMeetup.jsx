c1e03ba1d7215c0e7db425dcdb417a8d814c6165
niloysikdar <niloysikdar30@gmail.com>
2021-05-19 00:30:31 +0530

    Adding Data to Database

--- src/pages/NewMeetup.jsx
+++ src/pages/NewMeetup.jsx
@@ -1,11 +1,28 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 import classes from "./NewMeetup.module.css";
+import { useHistory } from "react-router-dom";
 
 const NewMeetupPage = () => {
+  const history = useHistory();
+
+  const onAddMeetup = (meetupData) => {
+    fetch(
+      "https://react-meetups-1eff3-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  };
   return (
     <div className={classes.container}>
       <h2>Add New Meetup</h2>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={onAddMeetup} />
     </div>
   );
 };
