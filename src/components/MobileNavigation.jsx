17d1c7690bfecff3f07fba5a76d1cdd4ebe2f54c
misztijanos <misztijanos@yahoo.com>
2021-05-24 11:57:51 +0300

    fixed >540px menu, added toggle button for <540px ... nothing to toggle yet

---
+++ src/components/MobileNavigation.jsx
@@ -0,0 +1,7 @@
+import classes from "./MobileNavigation.module.css";
+
+const MobileNavigation = () => {
+  return <button className={classes.mobile_navigation}>Menu</button>;
+};
+
+export default MobileNavigation;
